f6743b1d1c2db945d00b5d0261331bafde955f42
Nestor Espinola <nestorcde@gmail.com>
2018-02-05 23:43:21 -0300

    Practica Jaba Base de Datos

---
+++ src/pruebabd/ConexionBD2.java
@@ -0,0 +1,21 @@
+
+package pruebabd;
+
+import java.sql.*;
+
+public class ConexionBD2 {
+    
+    
+    Connection cn;
+    public Connection conexion1() {
+    try{
+    Class.forName("org.postgresql.Driver");
+    cn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/prueba_DB","postgres", "nestor210505");
+    
+    
+}catch(ClassNotFoundException | SQLException e){
+System.out.println("Error de SQL");
+}
+return cn;
+}
+}
